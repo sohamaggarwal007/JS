254abbc712bae95402a16cd2e5dac841b329262f
sohamaggarwal007 <sohamaggarwal007@gmail.com>
2026-02-11 21:52:58 +0530

    updated

---
+++ ArraysAndObjects/index.js
@@ -0,0 +1,32 @@
+// let obj = {
+//     name: "Soham",
+//     age: 18,
+//     height: "5ft 9inch",
+//     hobbies: "Coding",
+//     greet: function() {
+//         console.log("Hello How are U");
+//     }
+// };
+
+// console.log(obj);
+// obj.greet();
+// console.log(typeof(obj));
+
+// let obj2 = obj;
+// console.log(obj2);
+// obj2.greet();
+
+
+// CREATION OF ARRAYS
+
+// let arr = [1,2,3,4,5,];
+// console.log(arr);
+// // array constructor
+// let brr = new Array(1,"Soham",true);
+// console.log(brr[0]);
+// console.log(brr[1]);
+// console.log(brr[2]);
+// console.log(brr);
+// console.log(typeof(arr));
+// console.log(typeof(brr));
+
