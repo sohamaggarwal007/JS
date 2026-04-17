92f29501e522e69c343b85254b921a2eef2022d0
sohamaggarwal007 <sohamaggarwal007@gmail.com>
2026-04-17 14:59:25 +0530

    Updated

---
+++ JS_DOM_ChangingHTML/index.js
@@ -0,0 +1,14 @@
+// let mydiv = document.querySelector('#mydiv');
+
+// let newElement = document.createElement('span');
+// newElement.textContent = 'Soham Aggarwal';
+
+// mydiv.insertAdjacentElement('beforebegin', newElement); // Before beginning of the next div newElement is placed
+// mydiv.insertAdjacentElement('afterbegin', newElement); // At start of the next div newElement is placed
+// mydiv.insertAdjacentElement('beforeend', newElement); // newElement is placed just before ending of the div
+// mydiv.insertAdjacentElement('afterend', newElement); // newElement is placed after ending of the div
+
+
+// let parent = document.querySelector('#mydiv');
+// let child = document.querySelector('#fpara');
+// parent.removeChild(child);
